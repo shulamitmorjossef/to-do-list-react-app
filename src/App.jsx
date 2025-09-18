0e7699714bb65f2f01a7ddf860b3cadf30d005cb
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-09-18 13:39:39 +0300

    Added Abot page and Button

--- src/App.jsx
+++ src/App.jsx
@@ -4,63 +4,49 @@ import Sidebar from "./components/Sidebar.jsx";
 import Footer from "./components/Footer.jsx";
 import TodoList from "./components/TodoList.jsx";
 import EditModal from "./components/EditModal.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About.jsx";
+import "./App.css"; // הקובץ החיצוני לעיצוב
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [editingTodo, setEditingTodo] = useState(null);
 
-  const addTodo = (text) => {
-    setTodos([...todos, { id: Date.now(), text, done: false }]);
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  // const editTodo = (todo) => {
-  //   setEditingTodo(todo);
-  // };
-
+  const addTodo = (text) => setTodos([...todos, { id: Date.now(), text, done: false }]);
+  const toggleTodo = (id) => setTodos(todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo));
+  const deleteTodo = (id) => setTodos(todos.filter(todo => todo.id !== id));
   const saveTodo = (newText) => {
-    setTodos(
-      todos.map((t) =>
-        t.id === editingTodo.id ? { ...t, text: newText } : t
-      )
-    );
+    setTodos(todos.map(t => t.id === editingTodo.id ? { ...t, text: newText } : t));
     setEditingTodo(null);
   };
-
   const closeModal = () => setEditingTodo(null);
 
   return (
-    <div className="app-container">
-      <Header />
-      <div className="content">
-        <Sidebar addTodo={addTodo} />
-        <TodoList
-          todos={todos}
-          toggleTodo={toggleTodo}
-          deleteTodo={deleteTodo}
-          // editTodo={editTodo}
-        />
+    <Router>
+      <div className="top-bar">
+        <Link to="/about" className="about-button">About</Link>
       </div>
-      <Footer />
-
-      {/* <EditModal
-        isOpen={!!editingTodo}
-        currentText={editingTodo?.text || ""}
-        onClose={closeModal}
-        onSave={saveTodo}
-      /> */}
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="app-container">
+              <Header />
+              <div className="content">
+                <Sidebar addTodo={addTodo} />
+                <TodoList
+                  todos={todos}
+                  toggleTodo={toggleTodo}
+                  deleteTodo={deleteTodo}
+                />
+              </div>
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-page">
+      <h1>About This App</h1>
+      <p>This is a simple to-do list application built with React.</p>
+      <p>It demonstrates routing, editing tasks, and more!</p>
+    </div>
+  );
+}
+
+export default About;
